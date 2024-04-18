9b3262ba701d7be6d0cf5b4674c046199ef8ea5d
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-04-18 19:45:35 +0530

    Main Spring Boot Class

---
+++ MyCassandraApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.mycassandraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCassandraApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCassandraApiApplication.class, args);
+    }
+}
