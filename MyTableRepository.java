be7550de1fb4e31a2cea25d4d48252cbafd07e19
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-04-30 23:25:42 +0530

    Find Records in Given Age Range

--- MyTableRepository.java
+++ MyTableRepository.java
@@ -3,7 +3,9 @@ package com.example.mycassandraapi.repository;
 import com.example.mycassandraapi.model.MyTableModel;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface MyTableRepository extends CassandraRepository<MyTableModel, UUID> {
+    List<MyTableModel> findByAgeBetween(int minAge, int maxAge);
 }
